b67d4a6c8dbb18361342673705e557dfd7438764
ghaly26 <eec.ghali2012@gmail.com>
2026-06-02 21:07:10 -0500

    if-case sensitivity

--- # if condition_unit_02.py
+++ # if condition_unit_02.py
@@ -10,3 +10,21 @@ elif chair_number<13:
     print ("You win")
 else:
     print ("sorry")
+
+#========================================================#
+# if condition with string && case sensitivity
+#========================================================#
+area = input("Choose an area (Tanta, Cairo, Alexandria): \n")
+if area.upper()== "TANTA":
+    print("You chose Tanta!")
+    print("Tanta is nice!")
+elif area.lower()== "cairo":
+    print("You chose Cairo!")
+    print("Cairo is amazing!")
+elif area.lower() == "alexandria":
+    print("You chose alexandria!")
+    print("Alexandria feels like summer!")
+else:
+    print(f"{area} is not on our list!")
+
+#========================================================#
